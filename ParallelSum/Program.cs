7c7fa80f9ff26ddb13dca826eb8f77b9ab30b58a
Chernikov Nikita <14nik20@gmail.com>
2025-03-24 16:24:10 +0200

    Improved explanations, added PLINQ

--- ParallelSum/Program.cs
+++ ParallelSum/Program.cs
@@ -7,23 +7,33 @@ Parallel.For(0, arr.Length, i =>
 });
 Console.WriteLine($"Checksum: {arr.Sum()}");
 
+Console.WriteLine("Example:");
 var sw = Stopwatch.StartNew();
 Console.WriteLine(ParallelSum.ParallelSum.Sum(arr));
 sw.Stop();
 Console.WriteLine(sw.Elapsed);
+Console.WriteLine();
 
 sw.Restart();
-Console.WriteLine(ParallelSum.ParallelSumDivideAndConquer.Sum(arr));
+Console.WriteLine($"Parallel sum (divide and conquer): {ParallelSum.ParallelSumDivideAndConquer.Sum(arr)}");
 sw.Stop();
-Console.WriteLine(sw.Elapsed);
+Console.WriteLine($"Parallel sum (divide and conquer): {sw.Elapsed}");
+Console.WriteLine();
 
 sw.Restart();
-Console.WriteLine(ParallelSum.ParallelSum.Sum(arr));        // Looks like the best algorithm for this problem.
+Console.WriteLine($"Parallel sum: {ParallelSum.ParallelSum.Sum(arr)}");        // Looks like the best algorithm for this problem.
 sw.Stop();
-Console.WriteLine(sw.Elapsed);
+Console.WriteLine($"Parallel sum: {sw.Elapsed}");
+Console.WriteLine();
 
 sw.Restart();
-Console.WriteLine(ParallelSum.SimpleParallelSum.Sum(arr));        // Even slower than non-parallel LINQ.
-                                                                  // Do not use this.
+Console.WriteLine($"PLINQ: {arr.AsParallel().Sum()}");                  // A bit slower than manual implementations, but good.
 sw.Stop();
-Console.WriteLine(sw.Elapsed);
+Console.WriteLine($"PLINQ: {sw.Elapsed}");
+Console.WriteLine();
+
+sw.Restart();
+Console.WriteLine($"Simple sum: {ParallelSum.SimpleParallelSum.Sum(arr)}");        // Even slower than non-parallel LINQ.
+                                                                                   // Do not use this.
+sw.Stop();
+Console.WriteLine($"Simple sum: {sw.Elapsed}");
